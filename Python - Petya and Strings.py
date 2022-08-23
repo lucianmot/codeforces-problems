9c0624b2c20a482913d9efbc65624b8c7d0d0a6b
lucian <37814580+lucianmot@users.noreply.github.com>
2022-08-23 21:45:14 +0100

    Create Python - Petya and Strings.py

---
+++ Python - Petya and Strings.py
@@ -0,0 +1,12 @@
+# Python - Petya and Strings
+# https://codeforces.com/problemset/problem/112/A
+
+string_1 = input().lower()
+string_2 = input().lower()
+
+if string_1 < string_2:
+    print("-1")
+elif string_1 > string_2:
+    print("1")
+else:
+    print("0")
