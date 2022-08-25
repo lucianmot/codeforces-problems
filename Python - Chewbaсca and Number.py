d33b7296c7781877d301821193ebf39f9ca6b4e8
lucian <37814580+lucianmot@users.noreply.github.com>
2022-08-25 14:50:58 +0100

    Create Python - Chewbaсca and Number.py

---
+++ Python - Chewbaсca and Number.py
@@ -0,0 +1,19 @@
+# Python - Chewbaсca and Number
+# https://codeforces.com/problemset/problem/514/A
+
+number = input()
+arr = [int(x) for x in str(number)]
+answer = ""
+
+counter = 0
+
+for x in arr:
+    if x == 9 and counter == 0:
+        answer+=str(x)
+    elif 9 - x < x:
+        answer+=str(9-x)
+    else:
+        answer+=str(x)
+    counter+=1
+
+print(answer)
