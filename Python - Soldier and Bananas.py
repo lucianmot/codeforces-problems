e016c8378c4125932c3df120e0efe490498b16aa
lucian <37814580+lucianmot@users.noreply.github.com>
2022-08-26 07:55:14 +0100

    Update Python - Soldier and Bananas.py

--- Python - Soldier and Bananas.py
+++ Python - Soldier and Bananas.py
@@ -1,6 +1,7 @@
 # Python - Soldier and Bananas
 # https://codeforces.com/problemset/problem/546/A
 
+# 1st solution
 k, n, w = map(int, input().split())
 answer = 0
 for x in range(1, w+1):
@@ -8,4 +9,11 @@ for x in range(1, w+1):
 
 print(answer-n) if answer-n > 0 else print("0")
 
-k * (w * (w - 1)) 
+k * (w * (w - 1))
+
+# 2nd solution
+k, n, w = map(int, input().split());
+
+s = w * (w+1) / 2 * k - n;
+
+print (int(s)) if s > 0 else print (0);
