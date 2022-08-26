0c103cbabf6dfbf3b2c1e0e69021f9099bce8fb4
lucian <37814580+lucianmot@users.noreply.github.com>
2022-08-26 12:51:11 +0100

    Create Python - Football.py

---
+++ Python - Football.py
@@ -0,0 +1,12 @@
+# Python - Football
+# https://codeforces.com/problemset/problem/43/A
+
+from collections import Counter
+
+n = int(input())
+arr = []
+for x in range(n):
+    team = input()
+    arr.append(team)
+answer = dict(Counter(arr))
+print(max(answer, key=answer.get))
