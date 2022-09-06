cdcb28b6a3e9945a80cff278168e6e0e92500a73
lucian <37814580+lucianmot@users.noreply.github.com>
2022-09-06 20:57:07 +0100

    Create Python - In Search of an Easy Problem.py

---
+++ Python - In Search of an Easy Problem.py
@@ -0,0 +1,10 @@
+# Python - In Search of an Easy Problem
+# https://codeforces.com/problemset/problem/1030/A
+
+ignore = input()
+assess = str(input())
+
+if assess.count('1') == 0:
+    print("EASY")
+else:
+    print("HARD")
