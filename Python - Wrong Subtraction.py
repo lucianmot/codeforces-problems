3ee0aa356b00a9327eebc72f72655c9ffc05af21
lucian <37814580+lucianmot@users.noreply.github.com>
2022-09-06 20:34:09 +0100

    Create Python - Wrong Subtraction.py

---
+++ Python - Wrong Subtraction.py
@@ -0,0 +1,12 @@
+# Python - Wrong Subtraction
+# https://codeforces.com/problemset/problem/977/A
+
+n, k = list(map(int, input().split()))
+
+for i in range(k):
+    if n % 10 == 0:
+        n/=10
+    else:
+        n-=1
+
+print(int(n))
