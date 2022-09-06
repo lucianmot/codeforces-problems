616f04e7549d352de32ca6a0af5c2279a6b1d95f
lucian <37814580+lucianmot@users.noreply.github.com>
2022-09-06 20:48:55 +0100

    Create Python - Nearly Lucky Number.py

---
+++ Python - Nearly Lucky Number.py
@@ -0,0 +1,11 @@
+# Python - Nearly Lucky Number
+# https://codeforces.com/contest/110/problem/A
+
+n = str(input())
+
+nearLuckyN = n.count("4") + n.count("7")
+
+if nearLuckyN == 4 or nearLuckyN == 7:
+    print("YES")
+else:
+    print("NO")
