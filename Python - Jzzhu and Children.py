3bb0d47060ebfb0683fd4df5cc5e2333bb459d58
lucian <37814580+lucianmot@users.noreply.github.com>
2022-09-14 09:20:41 +0100

    Create Python - Jzzhu and Children.py

---
+++ Python - Jzzhu and Children.py
@@ -0,0 +1,19 @@
+# Python - Jzzhu and Children
+# https://codeforces.com/contest/450/problem/A
+
+import math;
+ 
+n, m = list(map(int, input().split(" ")));
+c = list(map(int, input().split(" ")));
+ 
+max = -1;
+r = -1;
+ 
+for i, v in enumerate(c):
+  x = math.ceil(v / m);
+  
+  if x >= max:
+    max = x;
+    r = i+1;
+ 
+print (r);
