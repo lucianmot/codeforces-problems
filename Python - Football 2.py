b7548c68bf5ea63cde37976e93842da5fa809004
lucian <37814580+lucianmot@users.noreply.github.com>
2022-09-19 19:35:48 +0100

    Create Python - Football 2.py

---
+++ Python - Football 2.py
@@ -0,0 +1,9 @@
+# Python - Football 2
+# https://codeforces.com/problemset/problem/96/A
+
+a_string = str(input())
+
+if "0000000" in a_string or "1111111" in a_string:
+    print("YES")
+else:
+    print("NO")
