9ae1d2ee0e7bfc49edf8df83b7c1e1a5ea5b7830
lucian <37814580+lucianmot@users.noreply.github.com>
2022-09-20 11:06:21 +0100

    Create Python - Even Odds.py

---
+++ Python - Even Odds.py
@@ -0,0 +1,21 @@
+# Python - Even Odds
+# https://codeforces.com/problemset/problem/318/A
+
+import math
+n, k = list(map(int, input().split(" ")))
+
+odds = []
+evens = []
+
+if k <= n / 2:
+    for i in range(1, n+1, 2):
+            odds.append(i)
+else:
+    for i in range(2, n+1, 2):
+            evens.append(i)
+        
+if k <= n / 2:
+    print(odds[k-1])
+else:
+    half_index = k - 1 - math.ceil(n/2)
+    print(evens[half_index])
