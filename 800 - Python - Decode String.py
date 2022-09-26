9cfea8c3a23a18eca08983681af4f4b1a71d4797
lucian <37814580+lucianmot@users.noreply.github.com>
2022-09-26 10:28:53 +0100

    Update and rename Python - Decode String.py to 800 - Python - Decode String.py

--- 800 - Python - Decode String.py
+++ 800 - Python - Decode String.py
@@ -1,4 +1,5 @@
 # Python - Decode String
+# Rating - 800
 # https://codeforces.com/contest/1729/problem/B
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
