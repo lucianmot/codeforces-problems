03b056cadc797b9b926fa6da8e8da3ac87df6f37
lucian <37814580+lucianmot@users.noreply.github.com>
2022-10-31 07:46:33 +0000

    Update Python - String Task.py

--- Python - String Task.py
+++ Python - String Task.py
@@ -1,5 +1,5 @@
 # Python - String Task
-# https://codeforces.com/problemset/problem/118/A
+# https://codeforces.com/problemset/problem/118/A   
 
 consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z", "w"]
 answer = ""
